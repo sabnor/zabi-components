85a26cf4ed62fa79c03a2a09516a13b872e18aa9
Sabina Nordell <sabinanordell@gmail.com>
2025-10-05 17:12:50 +0200

    chore: update generated files

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -63,6 +63,7 @@ declare module '$env/static/private' {
 	export const _: string;
 	export const __CFBundleIdentifier: string;
 	export const __CF_USER_TEXT_ENCODING: string;
+	export const VSCODE_L10N_BUNDLE_LOCATION: string;
 	export const ELECTRON_NO_ASAR: string;
 	export const NODE_ENV: string;
 }
@@ -131,6 +132,7 @@ declare module '$env/dynamic/private' {
 		_: string;
 		__CFBundleIdentifier: string;
 		__CF_USER_TEXT_ENCODING: string;
+		VSCODE_L10N_BUNDLE_LOCATION: string;
 		ELECTRON_NO_ASAR: string;
 		NODE_ENV: string;
 		[key: `PUBLIC_${string}`]: undefined;
